ef2605a55f66631385a513aa646b0b65c7a5d0ab
Lenilson Teixeira <lenilsonts@gmail.com>
2025-07-14 22:33:42 -0300

    ajustes

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -19,8 +19,8 @@ const SearchBar = ({ onSearch, searchQuery }: SearchBarProps) => {
   };
 
   return (
-    <form onSubmit={handleSearch} className='w-full mt-8 flex justify-center'>
-      <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
+    <form onSubmit={handleSearch} className='w-full flex justify-center'>
+      <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-4 mx-3 rounded-full w-3/4 sm:w-1/2'>
         <input
           type='text'
           className='flex-1 outline-none bg-inherit text-sm'
